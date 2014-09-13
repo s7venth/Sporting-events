b8813677ffeffa8ee4bc6e810717c17ef56a869e
zoubert <zouberthanem@gmail.com>
2014-09-13 19:26:58 +0200

    réctif bug build path

--- SportingEvents/src/fr/sportingevents/MainActivity.java
+++ SportingEvents/src/fr/sportingevents/MainActivity.java
@@ -1,14 +1,13 @@
 package fr.sportingevents;
-import android.support.v7.app.ActionBarActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
-
 
 public class MainActivity extends ActionBarActivity {
 	
