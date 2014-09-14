785ff7d47ed42e5932c4985a662fca9be43aa350
Sylvain <Sylvain@vincent>
2014-09-14 15:04:13 +0200

    -Ajout du fichier xml "champ_login_pressed" permetant de changer la forme des champs de la connexion - Modification de la classe "MainActivity pour prendre en compte le changement

--- SportingEvents/src/fr/sportingevents/MainActivity.java
+++ SportingEvents/src/fr/sportingevents/MainActivity.java
@@ -1,22 +1,57 @@
 package fr.sportingevents;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	
 	Button btn_connexion;
+	EditText champ_login,champ_mdp;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addOnclickListner();
+        
+        champ_login = (EditText) findViewById(R.id.login);
+        champ_mdp = (EditText) findViewById(R.id.mdp);
+        
+        /* Description : Permet de changer la forme du champ quand il est selectionné 
+         * cible : EditText Login et mot de passe
+         */
+        champ_login.setOnTouchListener(new View.OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				champ_login.setBackgroundResource(R.drawable.champ_login_pressed);
+				champ_mdp.setBackgroundResource(R.drawable.rightroundborder);
+				return false;
+			}
+		});
+        
+        /* Description : Permet de changer la forme du champ quand il est selectionné 
+         * cible : EditText Login et mot de passe
+         */
+        champ_mdp.setOnTouchListener(new View.OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View arg0, MotionEvent arg1) {
+				champ_mdp.setBackgroundResource(R.drawable.champ_login_pressed);
+				champ_login.setBackgroundResource(R.drawable.rightroundborder);
+				return false;
+			}
+		});
+        
     }
 
 
