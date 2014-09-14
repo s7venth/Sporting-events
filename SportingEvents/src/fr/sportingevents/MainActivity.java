1f454fbc3adde52bc997dcb16258ab68b57dd41e
Sylvain <Sylvain@Pc-de-Sylvain>
2014-09-14 16:53:35 +0200

    -Ajout d'image et de la barre de menu

--- SportingEvents/src/fr/sportingevents/MainActivity.java
+++ SportingEvents/src/fr/sportingevents/MainActivity.java
@@ -82,10 +82,12 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    	switch (item.getItemId()) {
+    	case R.id.action_option:
+    		// Comportement du bouton "A Propos"
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
     }
 }
