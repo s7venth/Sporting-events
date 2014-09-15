42aab58654dce1c82fcedc6e46a87cbce4855b1a
zoubert <zouberthanem@gmail.com>
2014-09-15 17:49:28 +0200

    Taille logo

--- SportingEvents/src/fr/sportingevents/Home_activity.java
+++ SportingEvents/src/fr/sportingevents/Home_activity.java
@@ -1,6 +1,8 @@
 package fr.sportingevents;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 /**
  * @author Zoubert Hanem
  * <p>Page d'accueil de l'application</p>
@@ -13,5 +15,18 @@ public class Home_activity extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		 getMenuInflater().inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
 	}
 }
